9ba882d6680d8b84d76f21aacdfe6c6a3417c354
Mstensnes <michael.stensnes@gmail.com>
2025-02-23 17:07:57 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Velkommen til XpressStyle!</h1>
+      <p>Rabatter og gode tilbud kommer snart.</p>
+    </div>
+  );
+}
